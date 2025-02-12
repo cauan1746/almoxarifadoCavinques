b184f8fdf0f14adebbc22723579ab670ef7a2b9e
JuhBeca <168589880+JuhBeca@users.noreply.github.com>
2025-02-12 13:56:01 -0300

    inicio itensentradaTable

---
+++ BackSRC/AlmoxarifadoBackSLN/AlmoxarifadoBackAPI/Controllers/ItensEntradaController.cs
@@ -0,0 +1,54 @@
+﻿using AlmoxarifadoBackAPI.DTO;
+using AlmoxarifadoBackAPI.Models;
+using AlmoxarifadoBackAPI.Repositorio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Schema;
+
+namespace AlmoxarifadoBackAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItensEntradaController : ControllerBase
+    {
+        private readonly IItensEntradaRepositorio _db;
+        private object itensentrada;
+
+        public ItensEntradaController(IItensEntradaRepositorio db)
+        {
+            _db = db;
+        }
+
+
+
+        [HttpGet("/listaitensentrada")]
+        public IActionResult listaItensEntrada()
+        {
+            return Ok(_db.GetAll());
+        }
+
+        [HttpPost("/ItensEntrada")]
+        public IActionResult listaItensEntrada(ItensEntradaDTO itensentrada)
+        {
+            return Ok(_db.GetAll().Where(x => x.IdItensEnt == itensentrada.IdItensEnt));
+        }
+
+        [HttpPost("/addItensEntrada")]
+        public IActionResult addItensEntrada(ItensEntradaCadastroDTO itensentrada)
+        {
+
+            var novoItensEntrada = new ItensEntrada()
+            {
+                Id = itensentrada.Id,
+                IdEnt = itensentrada.IdEnt,
+                Quantidade = itensentrada.Quantidade,
+                Preco = itensentrada.Preco,
+                Total = itensentrada.Total
+
+            };
+            //_categorias.Add(novaCategoria);
+            _db.Add(novoItensEntrada);
+            return Ok("Cadastro com Sucesso");
+        }
+    }
+}
